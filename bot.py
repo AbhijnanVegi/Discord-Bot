e4e38d8d068f13ba056fd1f57ca9e9fb31d744b0
Abhijnan Vegi <abhijnan.vegi@gmail.com>
2021-05-18 01:23:41 +0530

    Render md implemented

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import logging
 
 from discord.ext.commands.core import command
-from discord.message import Message
+import pypandoc, pdf2image
 
 # Logging
 logger = logging.getLogger('discord')
@@ -54,6 +54,22 @@ async def get_embed(msg:discord.Message):
     for attachment in msg.attachments:
         embed.set_image(attachment.url)
     return embed
+
+
+@bot.command()
+async def md(ctx,*,string:str):
+    imgs = await render_md(string)
+    for img in imgs:
+        img = img.crop(box=(300,300,1500,1500))
+        img.save("Trash/out.png")
+        await ctx.send(file=discord.File("Trash/out.png"))
+
+async def render_md(string:str):
+    pypandoc.convert_text(string,'pdf','md',outputfile="Trash/out.pdf")
+    images = pdf2image.convert_from_path("Trash/out.pdf")
+    return images
+
+
 
 with open("token",'r') as tokenfile:
     token = tokenfile.read()
